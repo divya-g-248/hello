d1871a91537b3c2f03dae123729bbc53c05d4415
divya-g-248 <57281528+divya-g-248@users.noreply.github.com>
2020-03-14 13:19:01 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>   
 int main()
 {
-	printf("Hello World\n");   // hello Richard
+	printf("Hello World\n");   // hello world
+	printf("Hello Divya\n");   // hello Divya
+	printf("How are you?\n");  // how are you?
  	exit(0);         // end of the program
 }
